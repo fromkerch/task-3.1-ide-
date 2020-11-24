7b4c8045e8d3d2848d23bae6abf2286f19dbc0c9
Pavel Smirnov <luq2009@yandex.ru>
2020-11-24 17:43:40 +0300

    added program of calculation bonuses

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 7542;
+        int spentRubles = 20;
+        int bonusMile = ticketPrice / spentRubles;
+                System.out.println(bonusMile);
+
+    }
+}
